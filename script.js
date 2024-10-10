823632bf282b5bd583fbdcf82593b60fae59fed3
HariomYadav-sudo <imhariom25@gmail.com>
2024-10-09 19:27:58 -0500

    New

---
+++ script.js
@@ -0,0 +1,29 @@
+let signUpBtn= document.querySelector('.signupbtn');
+let signInBtn= document.querySelector('.signinbtn');
+let nameField= document.querySelector('.namefield');
+let title= document.querySelector('.title');
+let underline= document.querySelector('.underline');
+let text=document.querySelector('.text');
+
+
+
+signInBtn.addEventListener('click',()=>{
+    nameField.style.maxHeight='0';
+    title.innerHtml='Sign in';
+    text.innerHtml='Lost Password';
+    signUpBtn.classList.add('disable');
+    signInBtn.classList.remove('disable');
+    underline.style.transform='translateX(35px)';
+
+});
+
+signUpBtn.addEventListener('click',()=>{
+    nameField.style.maxHeight='69px';
+    title.innerHtml='Sign up';
+   
+    signUpBtn.classList.remove('disable');
+
+    signInBtn.classList.add('disable');
+    underline.style.transform='translateX(0)';
+
+});
